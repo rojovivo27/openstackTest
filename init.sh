50d5439e674b8951d469b0336e7c7de7c6272222
Aldo Hernandez <rojovivo_27@hotmail.com>
2016-09-07 22:40:12 -0500

    modificacion init.sh

--- init.sh
+++ init.sh
@@ -8,9 +8,10 @@ sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libex
 echo mysql-server-5.1 mysql-server/root_password password pistones | debconf-set-selections
 echo mysql-server-5.1 mysql-server/root_password_again password pistones | debconf-set-selections
 apt-get install -y mysql-server
+dpkg --get-selections | grep mysql
 
 #Install php
-sudo apt-get install libapache2-mod-php5 php5 php5-mcrypt -y
+sudo apt-get install libapache2-mod-php5 php5 php5-mcrypt php5-mysql -y
 sudo service apache2 restart
 
 #Copying landing page
