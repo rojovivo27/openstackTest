6ce73c51717f75d8c1f9fbc1db434d699ff20479
Aldo Hernandez <rojovivo_27@hotmail.com>
2016-09-07 22:04:40 -0500

    modificacion init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+
+#Install Apache
 sudo apt-get update
 sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y
+
+
+#Install php
+
+sudo apt-get install libapache2-mod-php5 php5 php5-mcrypt
+sudo service apache2 restart
+
 
 #Copying landing page
 wget https://raw.githubusercontent.com/rojovivo27/openstackTest/master/my-landing-page.html
