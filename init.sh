715515d516a0a5d3659f4e7712519c3a123740a1
Aldo Hernandez <rojovivo_27@hotmail.com>
2016-09-07 21:14:53 -0500

    archivos iniciales

---
+++ init.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y
+
+
+# Download and Install the Latest Updates for the OS
+apt-get update && apt-get upgrade -y
+
+# Set the Server Timezone to CST
+echo "America/Mexico_City" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
+
+# Enable Ubuntu Firewall and allow SSH & MySQL Ports
+ufw enable
+ufw allow 22
+ufw allow 3306
+
+# Install essential packages
+apt-get -y install zsh htop
+
+# Install MySQL Server in a Non-Interactive mode. Default root password will be "root"
+echo "mysql-server-5.6 mysql-server/root_password password root" | sudo debconf-set-selections
+echo "mysql-server-5.6 mysql-server/root_password_again password root" | sudo debconf-set-selections
+apt-get -y install mysql-server-5.6
+
+
+# Run the MySQL Secure Installation wizard
+mysql_secure_installation
+
+sed -i 's/127\.0\.0\.1/0\.0\.0\.0/g' /etc/mysql/my.cnf
+mysql -uroot -p -e 'USE mysql; UPDATE `user` SET `Host`="%" WHERE `User`="root" AND `Host`="localhost"; DELETE FROM `user` WHERE `Host` != "%" AND `User`="root"; FLUSH PRIVILEGES;'
+
+service mysql restart
+
+#Installation of php
+sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql
+
+#restarting apache2
+sudo systemctl restart apache2
+
+#Copying landing page
+wget https://raw.githubusercontent.com/openstack-hackathon/training-shade-cucea/master/challenge1/my-landing-page.html
+sudo cp my-landing-page.html /var/www/html/index.html
+
+wget --user lamaredu --password 12.lamar.34$$ http://www.lamar.edu.mx/innova/info.txt
+sudo cp info.txt /var/www/html/info.php
