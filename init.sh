70ef59d8b5d78be6081180a886cfdf5cb8138347
Aldo Hernandez <rojovivo_27@hotmail.com>
2016-09-07 22:19:19 -0500

    modificacion init.sh

--- init.sh
+++ init.sh
@@ -11,6 +11,13 @@ sudo apt-get install libapache2-mod-php5 php5 php5-mcrypt -y
 sudo service apache2 restart
 
 
+#Install mysql
+
+echo mysql-server-5.1 mysql-server/root_password password pistones | debconf-set-selections
+echo mysql-server-5.1 mysql-server/root_password_again password pistones | debconf-set-selections
+apt-get install -y mysql-server
+
+
 #Copying landing page
 wget https://raw.githubusercontent.com/rojovivo27/openstackTest/master/my-landing-page.html
 sudo cp my-landing-page.html /var/www/html/index.html
